4029881198dcb749989910e4180c8a676fc7dbe7
mill9r <mill9r@gmail.com>
2018-06-08 14:09:56 +0300

    dev: zombie

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,12 @@ import {Character} from "./character";
 import {redraw} from "./utils";
 
 const fps = 30;
-const body = ['leftArm', 'legs', 'torso', 'rightArm', 'head', 'hair'];
-const person = new Character(body, 245, 185,'img');
+const hero = ['leftArm', 'legs', 'torso', 'rightArm', 'head', 'hair'];
+const person = new Character(hero, 245, 185,'img');
 const position = [[40,-42],[0,0],[0,-50],[-15,-42],[-10,-125],[-37,-138]];
-person.set('hair',redraw(person, position,'canvas', 490, 270),1000 / fps);
+person.set('hair',redraw(person, position,'canvasHero', 490, 270,'canvasDiv'),1000 / fps);
+
+const zombiePosition = [[-10,-42],[0,0],[25,-50],[20,-42],[-10,-125]];
+const zombie = ['leftArm', 'legs', 'torso', 'rightArm', 'head'];
+const zombiePerson = new Character(zombie, 245, 185,'img/zombie');
+zombiePerson.set('head',redraw(zombiePerson,zombiePosition,'canvasZombie',490,270,'canvasDiv'),1000/fps);
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,15 +1,16 @@
 export const loadImage = (name,path) => {
     const img = new Image();
     img.src = `./src/${path}/${name}.png`;
+    console.log(img.src);
     return img;
 };
 
-export const redraw = (obj,position, canvasId, width, height) => {
-    const canvas = document.createElement(canvasId);
+export const redraw = (obj,position, canvasId, width, height,div) => {
+    const canvas = document.createElement('canvas');
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
     canvas.setAttribute('id', canvasId);
-    const canvasDiv = document.getElementById('canvasDiv');
+    const canvasDiv = document.getElementById(div);
     canvasDiv.appendChild(canvas);
     const context = document.getElementById(canvasId).getContext('2d');
     //TODO use lodash foreach(zip)

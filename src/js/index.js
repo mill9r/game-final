88f5ac4731b31115e0a4c077851734ed685cd586
mill9r <mill9r@gmail.com>
2018-06-09 14:35:44 +0300

    dev: ball-demo

--- src/js/index.js
+++ src/js/index.js
@@ -23,3 +23,104 @@ const zombieId = 'canvasZombie';
 const zombieContainer = addCanvas(zombieId, 490, 270, gameField);
 setInterval(redraw.bind(null, zombiePerson, zombiePosition, zombieContainer,zombieEyes), milliseconds / fps);
 // zombiePerson.set('head',redraw(zombiePerson,zombiePosition,'canvasZombie',490,270,'canvasDiv'),1000/fps);
+
+
+var Ball = function() {
+
+    var x = 0,
+        y = 0,
+        speed = 0,
+        direction = 1,
+        rollTimer,
+        dirty = false,
+        diameter = 20,
+        color = "#DD3333",
+        highlightColor = "#fa6565";
+
+    rollTimer = setInterval(updateRoll, 1000/25);
+
+    function roll(pSpeed, pDirection){
+        speed = pSpeed;
+        if(pDirection){
+            direction = pDirection;
+        }
+
+    }
+    function stop() {
+        speed = 0;
+    }
+    function updateRoll() {
+        x += direction * speed;
+    }
+    function draw(context) {
+
+        var centerX = x,
+            centerY = y + (diameter + 10)/2 - 2,
+            width = (diameter + 30),
+            height = 6;
+
+        context.beginPath();
+        context.moveTo(centerX, centerY);
+        context.bezierCurveTo(centerX-width/2,centerY-height/2,
+            centerX-width/2,centerY+height/2,
+            centerX,centerY+height/2);
+        context.bezierCurveTo(centerX+width/2,centerY+height/2,
+            centerX+width/2,centerY-height/2,
+            centerX,centerY-height/2);
+        context.fillStyle = "#000000";
+        context.fill();
+        context.closePath();
+
+        context.beginPath();
+        context.moveTo(x, y - (diameter + 10)/2);
+        context.arc(x,y,(diameter + 10)/2,0,2*Math.PI,false);
+        context.fillStyle = "#000000";
+        context.fill();
+        context.closePath();
+
+        context.beginPath();
+        context.moveTo(x, y - diameter/2);
+        context.arc(x,y,diameter/2,0,2*Math.PI,false);
+        context.fillStyle = color;
+        context.fill();
+        context.closePath();
+
+        centerX = x + 3;
+        centerY = y - 3;
+        context.beginPath();
+        context.moveTo(centerX, centerY);
+        context.arc(centerX,centerY,diameter/3/2,0,2*Math.PI,false);
+        context.fillStyle = highlightColor;
+        context.fill();
+        context.closePath();
+    }
+    function getX(){
+        return x;
+    }
+    function setX(pX){
+        x = pX;
+    }
+    function getY(){
+        return y;
+    }
+    function setY(pY){
+        y = pY;
+    }
+    return {
+        getX:getX,
+        setX:setX,
+        getY:getY,
+        setY:setY,
+        roll:roll,
+        draw:draw,
+        stop:stop
+    };
+}
+
+var canvasWidth = 490, canvasHeight = 220;
+
+var ball = new Ball();
+ball.setX(canvasWidth);
+ball.setY(200);
+ball.roll(speed, -1);
+displayList.push(ball);
